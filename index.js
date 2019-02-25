74b834fcb7b0f300f21bcdc9b96bb2aa601a1d4c
huoyx <huoyx@fenbi.com>
2019-02-25 16:40:45 +0800

    restructure

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
 var path = require("path");
 
 module.exports = function(content) {
-  const defaultConfig = {
-    basePath: [],
-    rewritePath: undefined,
-    emit: true
-  };
-
-  const config = Object.assign(defaultConfig, this.query);
-
-  const fileName = path.basename(this.resourcePath);
-  let filePath = config.basePath
-    ? path.relative(config.basePath, this.context)
-    : path.resolve(this.context);
-
-  filePath = path.join(filePath, fileName);
-  filePath = JSON.stringify(filePath);
+  const basePath = (this.query && this.query.basePath) || [];
 
   this.addDependency(this.resourcePath);
 
   return (
     "const path = require('path');" +
     "const filePath = path.resolve(__dirname, " +
-    filePath +
+    JSON.stringify(
+      path.join(
+        path.relative(basePath, this.context),
+        path.basename(this.resourcePath)
+      )
+    ) +
     ");" +
     "try { global.process.dlopen(module, filePath); } " +
     "catch(exception) { throw new Error('Cannot open ' + filePath + ': ' + exception); };"
